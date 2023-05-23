3aa0724803291698f39252892a7ab6b38cfad62a
赵凯 <zhaokai@angelalign.com>
2023-05-23 14:44:14 +0800

    feat: init ui

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>Add</a-button>
   </div>
 </template>
+<script>
+  import { Button } from 'ant-design-vue';
+  const ButtonGroup = Button.Group;
 
+  export default {
+    components: {
+      AButton: Button,
+    },
+  };
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
