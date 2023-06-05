f2fdf38901b6ada584c875e4e474f66b65a583f5
zhaokai <820641374@qq.com>
2023-06-05 22:42:30 +0800

    feat: update

--- src/views/file/IndexView.vue
+++ src/views/file/IndexView.vue
@@ -1,17 +1,58 @@
 <template>
-    <div class="file">
-      <h1>file</h1>
-    </div>
-  </template>
-  <script>
-  </script>
-  <style lang="less">
-  @media (min-width: 1024px) {
-    .file {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
+  <div class="file">
+    <h1>file</h1>
+    <a-table :dataSource="dataSource" :columns="columns" />
+  </div>
+</template>
+<script>
+import { Button, Input, Table } from 'ant-design-vue'
+
+export default {
+  setup() {
+    return {
+      dataSource: [
+        {
+          key: '1',
+          name: '胡彦斌.mp4',
+          size: 32,
+          address: '西湖区湖底公园1号',
+        },
+        {
+          key: '2',
+          name: '胡彦祖',
+          size: 42,
+          address: '西湖区湖底公园1号',
+        },
+      ],
+
+      columns: [
+        {
+          title: '文件名',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: '文件大小',
+          dataIndex: 'size',
+          key: 'size',
+        },
+        {
+          title: '修改时间',
+          dataIndex: 'updateTime',
+          key: 'updateTime',
+        },
+      ],
     }
+  },
+  components: {
+    ATable: Table
   }
-  </style>
+}
+</script>
+<style lang="less">
+.file {
+  width: 100%;
+  height: 100%;
+}
+</style>
     
